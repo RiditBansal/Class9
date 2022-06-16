d30ff046cfa1f85173a94ece6cf0ef1a2060a90a
RiditBansal <107621592+RiditBansal@users.noreply.github.com>
2022-06-16 16:42:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var box;
+var box1;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+box1 = createSprite(300,200,30,30);
+box1.shapeColor="red"
+box.shapeColor="blue"
+}
+
+function draw() 
+{
+   background(30);
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
+
+
